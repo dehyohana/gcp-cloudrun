413eafeeb693ea4c1209e0a06c9c6c9730fa40cc
dehyohana <73350097+dehyohana@users.noreply.github.com>
2024-06-21 22:00:09 -0300

    Feature/pipeline
    * create base deploy pipeline

--- gcp/base/variables.tf
+++ gcp/base/variables.tf
@@ -19,8 +19,3 @@ variable "repository_id" {
   type        = string
   description = "The last part of the repository name to be created"
 }
-
-variable "repository_format" {
-  type        = string
-  description = "Repository format (e.g: DOCKER, OCI)"
-}
